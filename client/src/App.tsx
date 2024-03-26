dad571e0f8997b0705371daaea38bff1fd405c15
Shougata <shougatapaul46@gmail.com>
2024-03-26 20:18:12 +0600

    Created loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Search from './pages/search'
-import Cart from './pages/cart'
+import { lazy, Suspense } from 'react'
+import Loader from './components/loader';
+
+const Home = lazy(() => import('./pages/home'));
+const Search = lazy(() => import('./pages/search'));
+const Cart =  lazy(() => import('./pages/cart'));
+
 
 const App = () => {
   return (
     <Router>
       {/* Header */}
-       <Routes>
+
+      <Suspense fallback={<Loader />}>
+      <Routes>
         <Route path="/"  element={<Home />} />
         <Route path="/search"  element={<Search />} />
         <Route path="/cart"  element={<Cart />} />
        </Routes>
+      </Suspense>
     </Router>
   )
 }
